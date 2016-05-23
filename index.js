93b842f4436520a06cb42d112158d827eacf15e3
Stanislav Romanov <kaermorchen@gmail.com>
2016-05-23 17:18:01 +0300

    Added import alias - @import "ember-cli-bootstrap-4/..."

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 /* jshint node: true */
 'use strict';
 
+var path = require('path');
+var Funnel = require('broccoli-funnel');
+var mergeTrees = require('broccoli-merge-trees');
+
 module.exports = {
   name: 'ember-cli-bootstrap-4',
   included: function(app) {
     this._super.included(app);
+  },
+  treeForStyles: function treeForStyles(tree) {
+    var styleTrees = [];
+
+    if (this.app.project.findAddonByName('ember-cli-sass')) {
+      var sassTree = new Funnel(path.join(this.app.bowerDirectory, 'bootstrap/scss'), {
+        destDir: 'ember-cli-bootstrap-4'
+      });
+      styleTrees.push(sassTree);
+    }
+
+    if (tree) {
+      styleTrees.push(tree);
+    }
+
+    return mergeTrees(styleTrees, { overwrite: true });
   }
 };
