c37500a4f9242ba2bb59dc25c26b04d1a833a753
Stanislav Romanov <kaermorchen@gmail.com>
2017-05-13 10:12:44 +0300

    Update ember-cli to 2.13

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,14 +1,9 @@
-/*jshint node:true*/
-/* global require, module */
-var EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
+/* eslint-env node */
+const EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 
-module.exports = function (defaults) {
+module.exports = function(defaults) {
   var app = new EmberAddon(defaults, {
-    sassOptions: {
-      includePaths: [
-        'bower_components/bootstrap/scss'
-      ]
-    }
+    // Add options here
   });
 
   /*
