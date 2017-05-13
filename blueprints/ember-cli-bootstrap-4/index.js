db722a93cb20e633d7bfc985b0e808422e374071
Stanislav Romanov <kaermorchen@gmail.com>
2017-05-13 11:25:22 +0300

    Added app.scss in the afterIstall hook

--- blueprints/ember-cli-bootstrap-4/index.js
+++ blueprints/ember-cli-bootstrap-4/index.js
@@ -1,7 +1,29 @@
+/* eslint-env node */
+'use strict';
+
+const path = require('path');
+const fs = require('fs');
+
 module.exports = {
   normalizeEntityName: function () { },
 
   afterInstall: function () {
+    let importStatement = '\n@import "ember-cli-bootstrap-4/bootstrap";\n';
+    let stylePath = path.join('app', 'styles');
+    let file = path.join(stylePath, `app.scss`);
+
+    if (!fs.existsSync(stylePath)) {
+      fs.mkdirSync(stylePath);
+    }
+
+    if (fs.existsSync(file)) {
+      this.ui.writeLine(`Added import statement to ${file}`);
+      return this.insertIntoFile(file, importStatement, {});
+    } else {
+      fs.writeFileSync(file, importStatement);
+      this.ui.writeLine(`Created ${file}`);
+    }
+
     return this.addPackagesToProject([
       { name: 'bootstrap', target: '^v4.0.0-alpha.6' }
     ]);
