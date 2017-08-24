3f702249f5d918e49fc4ae84fb1520715428d306
Stanislav Romanov <kaermorchen@gmail.com>
2017-08-24 18:51:22 +0300

    Added style test

--- tests/acceptance/index-test.js
+++ tests/acceptance/index-test.js
@@ -1,6 +1,7 @@
+/* global $ */
 import { test } from 'qunit';
 import moduleForAcceptance from '../../tests/helpers/module-for-acceptance';
-import { visit, currentURL } from 'ember-native-dom-helpers';
+import { visit, find, currentURL } from 'ember-native-dom-helpers';
 
 moduleForAcceptance('Acceptance | index');
 
@@ -8,4 +9,6 @@ test('visiting /index', async function(assert) {
   await visit('/');
 
   assert.equal(currentURL(), '/');
+
+  assert.equal($(find('.navbar.bg-dark')).css('backgroundColor'), 'rgb(52, 58, 64)');
 });
