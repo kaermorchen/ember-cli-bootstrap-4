ebda4d09611ec81f0896ebaa6159b23557121c14
Stanislav Romanov <kaermorchen@gmail.com>
2018-12-02 11:39:21 +0300

    Fixed tests

--- tests/acceptance/index-test.js
+++ tests/acceptance/index-test.js
@@ -1,5 +1,5 @@
 import { module, test } from 'qunit';
-import { visit, currentURL, find, click } from '@ember/test-helpers';
+import { visit, currentURL, find } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
 
 module('Acceptance | index', function(hooks) {
@@ -13,9 +13,5 @@ module('Acceptance | index', function(hooks) {
     const navbar = find('.navbar.bg-dark');
 
     assert.equal(window.getComputedStyle(navbar, null).getPropertyValue('background-color'), 'rgb(52, 58, 64)');
-
-    await click('#dropdown01');
-
-    assert.dom('[aria-labelledby="dropdown01"]').hasClass('show');
   });
 });
