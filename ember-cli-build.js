8b7a332ec1615bb4958c914eade2536cceadadbf
Stanislav Romanov <kaermorchen@gmail.com>
2022-02-22 19:12:31 +0300

    Fix lint error

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,8 +5,8 @@ const EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 module.exports = function (defaults) {
   let app = new EmberAddon(defaults, {
     'ember-cli-bootstrap-4': {
-      js: null
-    }
+      js: null,
+    },
   });
 
   /*
