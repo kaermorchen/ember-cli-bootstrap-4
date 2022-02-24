c16b2c67ee8374797b3c380b12bd6d25c3896b86
Stanislav Romanov <kaermorchen@gmail.com>
2022-02-24 13:48:36 +0300

    Remove importing `bootstrap.js`

--- index.js
+++ index.js
@@ -3,85 +3,14 @@
 const path = require('path');
 const Funnel = require('broccoli-funnel');
 const mergeTrees = require('broccoli-merge-trees');
-const fastbootTransform = require('fastboot-transform');
 const resolve = require('resolve');
-const defaultOptions = {
-  js: [
-    'util',
-    'alert',
-    'button',
-    'carousel',
-    'collapse',
-    'dropdown',
-    'modal',
-    'tooltip',
-    'popover',
-    'scrollspy',
-    'tab',
-    'toast',
-  ],
-};
 
 module.exports = {
   name: require('./package').name,
 
-  included(app) {
+  included() {
     this._super.included.apply(this, arguments);
     this._ensureFindHost();
-
-    let vendorPath = `vendor/${this.name}`;
-    let options = Object.assign({}, defaultOptions, app.options[this.name]);
-
-    if (Array.isArray(options.js)) {
-      let host = this._findHost();
-
-      host.import({
-        development: path.join(vendorPath, 'popper.js'),
-        production: path.join(vendorPath, 'popper.min.js'),
-      });
-
-      host.import({
-        development: path.join(vendorPath, 'popper-utils.js'),
-        production: path.join(vendorPath, 'popper-utils.min.js'),
-      });
-
-      options.js.forEach(function (file) {
-        host.import(path.join(vendorPath, `${file}.js`));
-      });
-    }
-  },
-
-  treeForVendor() {
-    let popperPath = path.join(
-      this.resolvePackagePath('popper.js'),
-      'dist',
-      'umd'
-    );
-    let popperJs = fastbootTransform(
-      new Funnel(popperPath, {
-        files: [
-          'popper.js',
-          'popper.min.js',
-          'popper-utils.js',
-          'popper-utils.min.js',
-        ],
-        destDir: this.name,
-      })
-    );
-
-    let bootstrapPath = path.join(
-      this.resolvePackagePath('bootstrap'),
-      'js',
-      'dist'
-    );
-    let bootstrapJs = fastbootTransform(
-      new Funnel(bootstrapPath, {
-        files: defaultOptions.js.map((item) => `${item}.js`),
-        destDir: this.name,
-      })
-    );
-
-    return mergeTrees([popperJs, bootstrapJs]);
   },
 
   treeForStyles(tree) {
